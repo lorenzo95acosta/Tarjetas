9cfaa206eb82ebd30092f2641288d564295c69ae
lorenzo95acosta <88293738+lorenzo95acosta@users.noreply.github.com>
2023-09-03 19:32:39 -0300

    Funcionalidad de música

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,45 @@ var x = setInterval(function () {
 }, 1000);
 
 
-document.addEventListener("DOMContentLoaded", function(e){
-    console.log("DOM funciona")
+var audios = document.getElementById("audioPrueba");
+var playAudio = () => {
+  audios.play().catch((error) => {
+    console.log(
+      "La reproducción automática no está permitida. Haz clic en la página para reproducir el audio."
+    );
+    return false;
   });
+  $("#btnPlay").addClass("hidden");
+  $("#btnPausa").removeClass("hidden");
+  $("#btnPausa").addClass("pulse");
+};
+var pauseAudio = () => {
+  audios.pause();
+  $("#btnPausa").addClass("hidden");
+  $("#btnPlay").removeClass("hidden");
+  $("#btnPlay").addClass("vertical_shake");
+};
+
+var audios = document.getElementById("audio");
+
+var playAudio = () => {
+  audios.play().catch((error) => {
+    console.log(
+      "La reproducción automática no está permitida. Haz clic en la página para reproducir el audio."
+    );
+    return false;
+  });
+  document.getElementById("audioPlay").classList.add("esconder");
+  document.getElementById("audioPausa").classList.remove("esconder");
+};
+var pauseAudio = () => {
+  audios.pause();
+  document.getElementById("audioPausa").classList.add("esconder");
+  document.getElementById("audioPlay").classList.remove("esconder");
+};
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+});
+
+
